c6f121646f70932badffa2bdbdf895edc5bd30ca
felipefriserio <felipefriserio@gmail.com>
2017-11-30 16:30:53 -0200

    Comlpemento da explicação dada em sala de aula

--- src/br/com/fiap/java/web/anotacoes/Aula3.java
+++ src/br/com/fiap/java/web/anotacoes/Aula3.java
@@ -11,11 +11,30 @@ public class Aula3 {
 		 * -- Revisao
 		 * Servlet  = Request / Response
 		 * Filter   = Intercepta/Filtra requisicoes/ chamadas
-		 * Listener = Ouvindo eventos  
-		 *			Tipos: 
-		 * 				1- Contexto(variavel compartilhada para toda app, todos usuarios) 
-		 * 				2- Sessao(variaveis que pertencem a um determinado usuario)
-		 * 				3- Request (soh existe durante a request) 
+		 * Listener = - Permitem monitorar e reagir a eventos relacionados com o ciclo de vida dos seguintes objetos: 
+		 * 		1- Contexto(variavel compartilhada para toda app, todos usuarios) 
+		 * 		2- Sessao(variaveis que pertencem a um determinado usuario)
+		 * 		3- Request (soh existe durante a request) 
+		 * 
+		 * 	O container Web pode ser configurado para notificar listeners quando
+		 *		• Contexto é inicializado ou destruído;
+		 *		• Sessão é passivada ou ativada;
+		 *		• Request é criado ou destruído;
+		 *		• Atributos são adicionados ou removidos
+		 *
+		 *
+		 *	 Exemplos de utilização de listeners:
+		 *		• Inicializar um pool de conexões com BD quando iniciar o
+		 *		contexto;
+	 	 *		• Quando inicializa-se uma sessão, obtém conexão do pool e
+		 *		devolve quando fechar a sessão;
+		 *		• Quando o contexto é removido, as conexões são fechadas e
+		 *		o pool é destruído;
+		 *		• Monitorar atributos gravados na sessão, possivelmente
+		 *		para medir ou limitar uso de memória;
+		 *		• Inicialização / remoção de cache
+		 *		• etc;
+		 * 
 		 * JSP		= Parecido com PHP, nada mais que uma jsp que eh transformada em servlet, rodada no lado do servidor
 		 * 				
 		 * 			Diretivas	- cabecalho
